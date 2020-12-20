22cf6ff52251b5c71cc70b5127d635f228d38190
kangugau <kangugau@gmail.com>
2020-12-20 21:51:09 +0700

    home UI

---
+++ web-react/src/pages/City.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { makeStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
+
+export default function City() {
+  // const useStyles = makeStyles((theme) => ({
+  //   root: {
+  //     display: 'flex',
+  //   },
+  // }))
+  // const classes = useStyles()
+
+  return (
+    <React.Fragment>
+      <Typography variant="h1">Ha Noi</Typography>
+    </React.Fragment>
+  )
+}
