8236cb95a5c1a61bb0934a60dc7ac2ba09750d34
kangugau <kangugau@gmail.com>
2020-12-22 05:47:28 +0700

    get data from other cities

--- crawler/request/index.js
+++ crawler/request/index.js
@@ -1,14 +1,23 @@
 const attractions = require('./attractions')
 const tags = require('./tags')
 const reviews = require('./reviews')
+const cities = require('./cities')
 const cityIds = {
   hanoi: 293924,
   danang: 298085,
   phuquoc: 469418,
+  hochiminh: 293925,
+  hoian: 298082,
+  nhatrang: 293928,
+  dalat: 293922,
+  vungtau: 303946,
+  hue: 293926,
+  phanthiet: 298086,
 }
 module.exports = {
   attractions,
   tags,
   reviews,
+  cities,
   cityIds,
 }
